c407390a2cac04da6b956c0a8b6cc48cce0abfda
Fabricio <fabricio.melo@dcomp.ufs.br>
2024-02-29 14:37:19 -0300

    fix(table): remove TableRoot

--- src/app/components/table/index.tsx
+++ src/app/components/table/index.tsx
@@ -2,12 +2,10 @@ import { TableBody } from './TableBody';
 import { TableFooter } from './TableFooter';
 import { TableHeader } from './TableHeader';
 import { TableLimitSize } from './TableLimitSize';
-import { TableRoot } from './TableRoot';
 import { TableSearch } from './TableSearch';
 
 export const Table = {
     Header: TableHeader,
-    Root: TableRoot,
     Body: TableBody,
     Footer: TableFooter,
     LimitSize: TableLimitSize,
